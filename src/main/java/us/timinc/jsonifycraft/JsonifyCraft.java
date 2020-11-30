fa4621eeecea3afd9678f23b48f954095d6d65e6
Tim <tmetcalfe89@gmail.com>
2020-11-30 15:08:48 -0800

    Fixed versions.
    1.1.4

--- src/main/java/us/timinc/jsonifycraft/JsonifyCraft.java
+++ src/main/java/us/timinc/jsonifycraft/JsonifyCraft.java
@@ -20,7 +20,7 @@ import us.timinc.jsonifycraft.deserializers.GameDeserializer;
 public class JsonifyCraft {
     public static final String MODID = "jsonifycraft";
     public static final String NAME = "JsonifyCraft";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.4";
 
     private static final Logger LOGGER = LogManager.getLogger(NAME);
 
